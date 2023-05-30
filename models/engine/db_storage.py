10bdef644b135794c3a019b0c9e253ee9ec05544
Budabos <opiyocaroline20@gmail.com>
2023-05-30 20:13:57 +0000

    first storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,16 +77,8 @@ class DBStorage:
 
     def get(self, cls, id):
         """ retrieves """
-        if cls in classes.values() and id and type(id) == str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
-                    return value
+        if cls and id:
+            objs = self.all()
+            key = '{}.{}'.format(cls.__name__, id)
+            return objs.get(key)
         return None
-
-    def count(self, cls=None):
-        """ counts """
-        data = self.all(cls)
-        if cls in classes.values():
-            data = self.all(cls)
-        return len(data)
